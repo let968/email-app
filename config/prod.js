ef41bf80ec7535f5e00ad5ff0aee69fa30b36cad
Christian Letourneau <let968@gmail.com>
2019-06-17 19:48:37 -0400

    fix environment variables

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_ID,
+    googleClientSecret: process.env.GOOGLE_SECRET,
+    mongo: {
+        username: process.env.MONGO_USERNAME,
+        password: process.env.MONGO_PASSWORD
+    },
+    cookieKey: process.env.COOKIE_KEY
+}
