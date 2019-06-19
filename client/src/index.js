c8ea388da30e6b1e5c719aa5fde303b96da444ef
Christian Letourneau <let968@gmail.com>
2019-06-18 23:11:03 -0400

    Add css module

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,17 @@
-import { React } from 'react';
-import { ReactDOM } from 'react-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render();
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
